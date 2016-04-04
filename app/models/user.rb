6022bf249d971c18497a50eb2f65a7ead87b4045
Katie <katelyn.hertel19@gmail.com>
2016-04-04 14:16:50 -0400

    Assignment 40 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,20 @@
+
 class User < ActiveRecord::Base
-   has_many :posts
+  has_many :posts
 
-   before_save { self.email = email.downcase }
-   before_save { self.role ||= :member }
+  before_save { self.email = email.downcase }
+  before_save { self.role ||= :member }
 
-   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-  validates :password, length: { minimum: 6 }, allow_blank: true
+  validates :password, length: { minimum: 6}, allow_blank: true
 
-   validates :email,
-             presence: true,
-             uniqueness: { case_sensitive: false },
-             length: { minimum: 3, maximum: 254 }
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 100 }
 
-   has_secure_password
+  has_secure_password
 
-   enum role: [:member, :admin]
+  enum role: [:member, :moderator, :admin]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,63 +1,72 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'random_data'
+include RandomData
 
-5.times do
-  User.create!(
-  name:     RandomData.random_name,
-  email:    RandomData.random_email,
-  password: RandomData.random_sentence
-  )
-end
-users = User.all
+#Create Users
 
-15.times do
-   Topic.create!(
-     name:         RandomData.random_sentence,
-     description:  RandomData.random_paragraph
-   )
- end
- topics = Topic.all
+  5.times do
+    user = User.create!(
+      name:     RandomData.random_name,
+      email:    RandomData.random_email,
+      password: RandomData.random_sentence
+    )
+  end
+  users = User.all
+# Create topics
+  15.times do
+    Topic.create!(
+      name:         RandomData.random_sentence,
+      description:  RandomData.random_paragraph
+    )
+  end
+  topics = Topic.all
 
- 50.times do
-   Post.create!(
-    user:   users.sample,
-    topic: topics.sample,
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
 
- 100.times do
-   Comment.create!(
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
+# Create posts
+  50.times do
 
- # Create an admin user
+    Post.create!(
+      user:  users.sample,
+      topic: topics.sample,
+      title: RandomData.random_sentence,
+      body:  RandomData.random_paragraph
+    )
+  end
+  posts = Post.all
+
+  #Create comments
+
+  100.times do
+    Comment.create!(
+
+      post: posts.sample,
+      body:  RandomData.random_paragraph
+    )
+  end
+
+  # Create an admin user
   admin = User.create!(
-    name:     'Admin User',
-    email:    'admin@example.com',
-    password: 'helloworld',
-    role:     'admin'
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
   )
 
-  # Create a member
+  #Create a member
   member = User.create!(
-    name:     'Member User',
-    email:    'member@example.com',
-    password: 'helloworld'
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
   )
 
- puts "Seed finished"
- puts "#{User.count} users created"
- puts "#{Topic.count} topics created"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+  # Create an moderator user
+  admin = User.create!(
+  name:     'Moderator User',
+  email:    'mod@example.com',
+  password: 'helloworld',
+  role:     'moderator'
+  )
+
+puts "Seed finished"
+puts "#{User.count} users created."
+puts "#{Topic.count} topics created."
+puts "#{Post.count} posts created."
+puts "#{Comment.count} comments created."
