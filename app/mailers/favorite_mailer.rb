0790d0fe25b8eb413d5f3c7759dedba846dc1906
Katie <katelyn.hertel19@gmail.com>
2016-04-06 16:31:15 -0400

    Checkpoint 44 updated still needs help

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,12 +1,12 @@
 class FavoriteMailer < ApplicationMailer
-  default from: "youremail.com"
+  default from: "katelyn.hertel19@gmail.com"
 
   def new_comment(user, post, comment)
 
 
-    headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
-    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+    headers["Message-ID"] = "<comments/#{comment.id}@frozen-reaches-64832.herokuapp.com>"
+    headers["In-Reply-To"] = "<post/#{post.id}@frozen-reaches-64832.herokuapp.com>"
+    headers["References"] = "<post/#{post.id}@frozen-reaches-64832.herokuapp.com>"
 
     @user = user
     @post = post
