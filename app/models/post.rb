4fbb6d3cf10f5a4d3d067a960522255a9fd17ebf
Katie <katelyn.hertel19@gmail.com>
2016-04-06 16:02:29 -0400

    Checkpoint 44 needs help

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
   default_scope { order('rank DESC') }
