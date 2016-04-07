c9ee4e37eb0c4993fbb149a31b5a10b2eb1459b4
Katie <katelyn.hertel19@gmail.com>
2016-04-07 16:50:22 -0400

    Assignment 42 complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,69 +2,77 @@ class CommentsController < ApplicationController
   before_action :require_sign_in
   before_action :authorize_user, only: [:destroy]
 
-   def create
-     @post = Post.find(params[:post_id])
-     @topic = Topic.find(params[:post_id])
-
-     comment.user = current_user
-
-   if @post
-     comment = @post.comments.new(comment_params)
-     if comment.save
-       flash[:notice] = "Comment saved successfully."
-       redirect_to [@post.topic, @post]
-     else
-       flash[:alert] = "Comment failed to save."
-       redirect_to [@post.topic, @post]
-     end
-   elsif @topic
-     comment = @topic.comments.new(comment_params)
-     if comment.save
-       flash[:notice] = "Comment saved successfully."
-       redirect_to @topic
-     else
-       flash[:alert] = "Comment failed to save."
-       redirect_to @topic
-     end
-   end
-
-   def destroy
-     @post = Post.find(params[:post_id])
-     @topic = Topic.find(params[:post_id])
-     comment = @post.comments.find(params[:id])
-     comment = @topic.comments.find(params[:id])
-
-    if @post
+  def create
+    if params[:post_id]
+      @post = Post.find(params[:post_id])
       comment = @post.comments.new(comment_params)
-     if comment.destroy
-       flash[:notice] = "Comment was deleted."
-       redirect_to [@post.topic, @post]
-     else
-       flash[:alert] = "Comment couldn't be deleted. Try again."
-       redirect_to [@post.topic, @post]
-     elsif @topic
-       comment = @topic.comments.new(comment_params)
-       if comment.destroy
-         flash[:notice] = "Comment was deleted."
-         redirect_to @topic
-       else
-         flash[:alert] = "Comment couldn't be deleted. Try again."
-         redirect_to @topic
-     end
-   end
+      comment.user = current_user
 
-   private
- # #14
-   def comment_params
-     params.require(:comment).permit(:body)
-   end
+      if comment.save
+        flash[:notice] = "Comment saved successfully."
+        redirect_to [@post.topic, @post]
+      else
+        flash[:alert] = "Comment failed to save."
+        redirect_to [@post.topic, @post]
+      end
 
+    elsif params[:topic_id]
+      @topic = Topic.find(params[:topic_id])
+      comment = @topic.comments.new(comment_params)
+      comment.user = current_user
+
+      if comment.save
+        flash[:notice] = "Comment saved successfully."
+        redirect_to @topic
+      else
+        flash[:alert] = "Comment failed to save."
+        redirect_to @topic
+      end
+    end
+  end
+
+  def destroy
+    if params[:post_id]
+      @post = Post.find(params[:post_id])
+      comment = @post.comments.find(params[:id])
+
+      if comment.destroy
+        flash[:notice] = "Comment was deleted."
+        redirect_to [@post.topic, @post]
+      else
+        flash[:alert] = "Comment couldn't be deleted. Try again."
+        redirect_to [@post.topic, @post]
+      end
+
+    elsif params[:topic_id]
+      @topic = Topic.find(params[:topic_id])
+      comment = @topic.comments.find(params[:id])
+
+      if comment.destroy
+        flash[:notice] = "Comment was deleted."
+        redirect_to @topic
+      else
+        flash[:alert] = "Comment couldn't be deleted. Try again."
+        redirect_to @topic
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 
   def authorize_user
     comment = Comment.find(params[:id])
     unless current_user == comment.user || current_user.admin?
       flash[:alert] = "You do not have permission to delete a comment."
-      redirect_to [comment.post.topic, comment.post]
+      if params[:post_id]
+        redirect_to [comment.post.topic, comment.post]
+      elsif params[:topic_id]
+        redirect_to comment.topic
+      end
     end
   end
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,111 +2,190 @@ require 'rails_helper'
 include SessionsHelper
 
 RSpec.describe CommentsController, type: :controller do
-  let(:my_user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
-   let(:other_user) { User.create!(name: RandomData.random_name, email: RandomData.random_email, password: "helloworld", role: :member) }
-   let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
-   let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user) }
-   let(:my_comment) { Comment.create!(body: 'Comment Body', post: my_post, user: my_user) }
+  let(:my_user) { User.create!(name: 'Bloccit User', email: 'user@bloccit.com', password: 'helloworld') }
+  let(:other_user) { User.create!(name: RandomData.random_name, email: RandomData.random_email, password: 'helloworld', role: 'member') }
+  let(:my_topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user) }
+  let(:my_comment) { Comment.create!(body: 'Comment Body', post: my_post, topic: my_topic, user: my_user) }
 
-   context "guest" do
-     describe "POST create" do
-       it "redirects the user to the sign in view" do
-         post :create, post_id: my_post.id, comment: {body: RandomData.random_paragraph}
-         expect(response).to redirect_to(new_session_path)
-       end
-     end
+  context "guest" do
+    describe "POST create" do
+      it "redirects the user to the sign in view" do
+        post :create, post_id: my_post.id, comment: {body: RandomData.random_paragraph}
 
-     describe "DELETE destroy" do
-       it "redirects the user to the sign in view" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
-         expect(response).to redirect_to(new_session_path)
-       end
-     end
-   end
+        expect(response).to redirect_to(new_session_path)
+      end
+    end
 
-   context "member user doing CRUD on a comment they don't own" do
-     before do
-       create_session(other_user)
-     end
+    describe "DELETE destroy" do
+      it "redirects the user to the sign in view" do
+        delete :destroy, post_id: my_post.id, id: my_comment.id
 
-     describe "POST create" do
-       it "increases the number of comments by 1" do
-         expect{ post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)
-       end
+        expect(response).to redirect_to(new_session_path)
+      end
+    end
+  end
 
-       it "redirects to the post show view" do
-         post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
-         expect(response).to redirect_to [my_topic, my_post]
-       end
-     end
+  context "member doing CRUD on a comment they don't own" do
+    before do
+      create_session(other_user)
+    end
 
-     describe "DELETE destroy" do
-       it "redirects the user to the posts show view" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
-         expect(response).to redirect_to([my_topic, my_post])
-       end
-     end
-   end
+    describe "POST create comment on post" do
+      it "increases the number of comments by 1" do
+        expect{ post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment, :count).by(1)
+      end
 
-   context "member user doing CRUD on a comment they own" do
-     before do
-       create_session(my_user)
-     end
+      it "redirects to the post show view" do
+        post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
 
-     describe "POST create" do
-       it "increases the number of comments by 1" do
-         expect{ post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)
-       end
+        expect(response).to redirect_to [my_topic, my_post]
+      end
+    end
 
-       it "redirects to the post show view" do
-         post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
-         expect(response).to redirect_to [my_topic, my_post]
-       end
-     end
+    describe "POST create comment on topic" do
+      it "increases the number of comments by 1" do
+        expect{ post :create, topic_id: my_topic.id, comment: {body: RandomData.random_sentence} }.to change(Comment, :count).by(1)
+      end
 
-     describe "DELETE destroy" do
-       it "deletes the comment" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
-         count = Comment.where({id: my_comment.id}).count
-         expect(count).to eq 0
-       end
+      it "redirects to the topic show view" do
+        post :create, topic_id: my_topic.id, comment: {body: RandomData.random_sentence}
 
-       it "redirects to the post show view" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
-         expect(response).to redirect_to [my_topic, my_post]
-       end
-     end
-   end
+        expect(response).to redirect_to my_topic
+      end
+    end
 
-   context "admin user doing CRUD on a comment they don't own" do
-     before do
-       other_user.admin!
-       create_session(other_user)
-     end
+    describe "DELETE destroy comment on post" do
+      it "redirects the user to the posts show view" do
+        delete :destroy, post_id: my_post.id, id: my_comment.id
 
-     describe "POST create" do
-       it "increases the number of comments by 1" do
-         expect{ post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)
-       end
+        expect(response).to redirect_to [my_topic, my_post]
+      end
+    end
 
-       it "redirects to the post show view" do
-         post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
-         expect(response).to redirect_to [my_topic, my_post]
-       end
-     end
+    describe "DELETE destroy comment on topic" do
+      it "redirects the user to the topic show view" do
+        delete :destroy, topic_id: my_topic.id, id: my_comment.id
 
-     describe "DELETE destroy" do
-       it "deletes the comment" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
-         count = Comment.where({id: my_comment.id}).count
-         expect(count).to eq 0
-       end
+        expect(response).to redirect_to my_topic
+      end
+    end
+  end
 
-       it "redirects to the post show view" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
-         expect(response).to redirect_to [my_topic, my_post]
-       end
-     end
-   end
+  context "member doing CRUD on their own comment" do
+    before do
+      create_session(my_user)
+    end
 
+    describe "POST create comment on post" do
+      it "increases the number of comments by 1" do
+        expect{ post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment, :count).by(1)
+      end
+
+      it "redirects to the post show view" do
+        post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
+
+        expect(response).to redirect_to [my_topic, my_post]
+      end
+    end
+
+    describe "POST create comment on topic" do
+      it "increases the number of comments by 1" do
+        expect{ post :create, topic_id: my_topic.id, comment: {body: RandomData.random_sentence} }.to change(Comment, :count).by(1)
+      end
+
+      it "redirects to the topic show view" do
+        post :create, topic_id: my_topic.id, comment: {body: RandomData.random_sentence}
+
+        expect(response).to redirect_to my_topic
+      end
+    end
+
+    describe "DELETE destroy comment on post" do
+      it "deletes the comment" do
+        delete :destroy, post_id: my_post.id, id: my_comment.id
+        count = Comment.where({id: my_comment.id}).count
+        expect(count).to eq 0
+      end
+
+      it "redirects the user to the posts show view" do
+        delete :destroy, post_id: my_post.id, id: my_comment.id
+
+        expect(response).to redirect_to [my_topic, my_post]
+      end
+    end
+
+    describe "DELETE destroy comment on topic" do
+      it "deletes the comment" do
+        delete :destroy, topic_id: my_topic.id, id: my_comment.id
+        count = Comment.where({id: my_comment.id}).count
+        expect(count).to eq 0
+      end
+
+      it "redirects the user to the topic show view" do
+        delete :destroy, topic_id: my_topic.id, id: my_comment.id
+
+        expect(response).to redirect_to my_topic
+      end
+    end
+  end
+
+  context "admin user doing CRUD on a comment they don't own" do
+    before do
+      other_user.admin!
+      create_session(other_user)
+    end
+
+    describe "POST create comment on post" do
+      it "increases the number of comments by 1" do
+        expect{ post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment, :count).by(1)
+      end
+
+      it "redirects to the post show view" do
+        post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
+
+        expect(response).to redirect_to [my_topic, my_post]
+      end
+    end
+
+    describe "POST create comment on topic" do
+      it "increases the number of comments by 1" do
+        expect{ post :create, topic_id: my_topic.id, comment: {body: RandomData.random_sentence} }.to change(Comment, :count).by(1)
+      end
+
+      it "redirects to the topic show view" do
+        post :create, topic_id: my_topic.id, comment: {body: RandomData.random_sentence}
+
+        expect(response).to redirect_to my_topic
+      end
+    end
+
+    describe "DELETE destroy comment on post" do
+      it "deletes the comment" do
+        delete :destroy, post_id: my_post.id, id: my_comment.id
+        count = Comment.where({id: my_comment.id}).count
+        expect(count).to eq 0
+      end
+
+      it "redirects the user to the posts show view" do
+        delete :destroy, post_id: my_post.id, id: my_comment.id
+
+        expect(response).to redirect_to [my_topic, my_post]
+      end
+    end
+
+    describe "DELETE destroy comment on topic" do
+      it "deletes the comment" do
+        delete :destroy, topic_id: my_topic.id, id: my_comment.id
+        count = Comment.where({id: my_comment.id}).count
+        expect(count).to eq 0
+      end
+
+      it "redirects the user to the topic show view" do
+        delete :destroy, topic_id: my_topic.id, id: my_comment.id
+
+        expect(response).to redirect_to my_topic
+      end
+    end
+  end
 end
