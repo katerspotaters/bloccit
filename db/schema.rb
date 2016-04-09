a4efa2978a672daf5d2c144457ffaf6417e11e1a
Katie <katelyn.hertel19@gmail.com>
2016-04-09 15:02:25 -0400

    Checkpoint 38 Complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406182052) do
+ActiveRecord::Schema.define(version: 20160409183813) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -19,9 +19,11 @@ ActiveRecord::Schema.define(version: 20160406182052) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "topic_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["topic_id"], name: "index_comments_on_topic_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "favorites", force: :cascade do |t|
@@ -79,6 +81,7 @@ ActiveRecord::Schema.define(version: 20160406182052) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
